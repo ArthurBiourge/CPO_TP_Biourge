9f64a8ce672d4c5396c053a110ea3ba34b7297ec
Arthur <arthur.biourge@epfedu.fr>
2025-10-24 11:17:05 +0200

    TP1 exercice 3 etape 2 pas fini

---
+++ TP1_guessMyNumber_Biourge/src/tp1_guessmynumber_biourge/TP1_guessMyNumber_Biourge.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_biourge;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author utilisateur
+ */
+public class TP1_guessMyNumber_Biourge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TP1 exercice 3, Arthur Biourge, 24/10/2025 
+        int n;
+        int i = 0;
+        int choice;
+        Scanner sc = new Scanner(System.in);
+        Random generateurAleat = new Random();
+        n = generateurAleat.nextInt(100);
+        System.out.println("Saissisez un nombre");
+        choice = sc.nextInt();
+        while (choice != n) {
+            
+            if (choice < n) {
+                System.out.println("Trop petit");
+                            System.out.println("Saissisez un nombre");
+                choice = sc.nextInt();
+                i = i + 1;
+            } else if (choice > n) {
+                System.out.println("Trop grand");
+                i = i + 1;
+            } else if (choice == n) {
+                System.out.println("Bravo vous avez trouvee");
+                System.out.println("Nombre d'essais total: " + i);
+            }
+
+        }
+    }
+
+}
