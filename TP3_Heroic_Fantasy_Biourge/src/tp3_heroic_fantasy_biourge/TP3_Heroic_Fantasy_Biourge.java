9fee7c9694988899f9530031acbbf1ec9496ecae
Arthur <arthur.biourge@epfedu.fr>
2025-11-12 12:53:43 +0100

    TP1 exercice 1 partie 3 sans array pas fini

---
+++ TP3_Heroic_Fantasy_Biourge/src/tp3_heroic_fantasy_biourge/TP3_Heroic_Fantasy_Biourge.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heroic_fantasy_biourge;
+
+/**
+ *
+ * @author utilisateur
+ */
+public class TP3_Heroic_Fantasy_Biourge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TP3 exercice 1 ; 12/11/2025; Arthur Biourge
+        Epee Epee1=new Epee("Excalibur",7,5);
+        Epee Epee2=new Epee("Durandal",4,7);
+        Baton Baton1= new Baton("Chêne", 4, 5);
+        Baton Baton2= new Baton( "Charme", 5, 6);
+    }
+    
+}
