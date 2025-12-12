9bec158c2e82be09291d59b3ca99dc080949e1c8
Arthur <arthur.biourge@epfedu.fr>
2025-12-12 01:12:28 +0100

    Mini projet: méthode de création de combinaison crée

--- miniprojet_cadenas_BIOURGE/src/miniprojet_cadenas_biourge/Cadenas.java
+++ miniprojet_cadenas_BIOURGE/src/miniprojet_cadenas_biourge/Cadenas.java
@@ -4,6 +4,8 @@
  */
 package miniprojet_cadenas_biourge;
 
+import java.util.Random;
+
 /**
  *
  * @author utilisateur
@@ -11,8 +13,17 @@ package miniprojet_cadenas_biourge;
 public class Cadenas {
 
     private int[] tabPropose = new int[4];
-    private int[] tabChercher = new int[4];
+    private int[] tabCombinaison = new int[4];
     int Valeur;
+    int i;
+    
+    public void Combinaison(){
+        Random rand = new Random();
+        for(i=0; i<4;i++){
+        tabCombinaison[i]= rand.nextInt(10);// commande me permettant d'obtenir un nombre  aléatoire entre 0 et 9
+        }
+    }
+
 
     public int mise_a_jour(int Valeur_case, boolean up) {
         if (up == true) {
