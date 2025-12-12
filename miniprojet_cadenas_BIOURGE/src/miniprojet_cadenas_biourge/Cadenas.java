651e47cbaec3c6b3a371aeb59fcde2b502ac529a
Arthur <arthur.biourge@epfedu.fr>
2025-12-12 13:39:42 +0100

    Mini projet: méthode test et interface finie

--- miniprojet_cadenas_BIOURGE/src/miniprojet_cadenas_biourge/Cadenas.java
+++ miniprojet_cadenas_BIOURGE/src/miniprojet_cadenas_biourge/Cadenas.java
@@ -12,18 +12,24 @@ import java.util.Random;
  */
 public class Cadenas {
 
-    private int[] tabPropose = new int[4];
     private int[] tabCombinaison = new int[4];
     int Valeur;
     int i;
+    int j;
+    int Valeur_exacte;
+    int Valeur_inférieur;
+    int Valeur_supérieur;
+    int Valeur_vérifier;
+    int exact;
     
-    public void Combinaison(){
-        Random rand = new Random();
-        for(i=0; i<4;i++){
-        tabCombinaison[i]= rand.nextInt(10);// commande me permettant d'obtenir un nombre  aléatoire entre 0 et 9
-        }
-    }
 
+    public void Combinaison() {
+        Random rand = new Random();
+        for (i = 0; i < 4; i++) {
+            tabCombinaison[i] = rand.nextInt(10);// commande me permettant d'obtenir un nombre  aléatoire entre 0 et 9
+        }
+
+    }
 
     public int mise_a_jour(int Valeur_case, boolean up) {
         if (up == true) {
@@ -40,5 +46,36 @@ public class Cadenas {
 
         return Valeur;
     }
-    
-}
+
+    public int[] Tester(int[] tab_proposé) {
+        int[] tab_retour= new int[4];
+        Valeur_exacte = 0;
+        Valeur_inférieur = 0;
+        Valeur_supérieur = 0;
+        
+        for (j = 0; j < 4; j++) {
+            if (tab_proposé[j] == tabCombinaison[j]) {
+                Valeur_exacte += 1;
+            }
+            else if (tab_proposé[j] < tabCombinaison[j]) {
+                Valeur_inférieur +=1;
+            }
+            else if(tab_proposé[j]>tabCombinaison[j]){
+                Valeur_supérieur +=1;
+            } 
+        }
+        if (Valeur_vérifier != 4){
+        exact=0;
+        }
+        else {
+            exact=1;
+        }
+        tab_retour[0]= Valeur_exacte;
+        tab_retour[1]= Valeur_inférieur;
+        tab_retour[2]= Valeur_supérieur;
+        tab_retour[3]= exact;
+        
+        return tab_retour;
+        }
+
+    }
