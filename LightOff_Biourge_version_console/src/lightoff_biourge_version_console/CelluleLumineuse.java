d5105cc89c1e58a408f389ccb00c2e70d328fd81
Arthur <arthur.biourge@epfedu.fr>
2025-12-17 10:53:56 +0100

    Mini Projet 2 "Lightoff": classe Cellule lumineuse "Terminer" - Méthode eteindreCellule(), estEteint(), getEtat() crées -Affichage toString en rapport avec Cellule lumineuse pour renvoyer du code

--- LightOff_Biourge_version_console/src/lightoff_biourge_version_console/CelluleLumineuse.java
+++ LightOff_Biourge_version_console/src/lightoff_biourge_version_console/CelluleLumineuse.java
@@ -9,20 +9,60 @@ package lightoff_biourge_version_console;
  * @author utilisateur
  */
 public class CelluleLumineuse {
+
     private boolean etat;
 
     public CelluleLumineuse(boolean etat_reçu) {
         this.etat = etat_reçu;
-        etat=false;
+        etat = false;
     }
-    public boolean activerCellule(boolean etat_reçu ){
-        if (etat_reçu == true){
-            etat=false;
+
+    public void activerCellule(boolean etat_reçu_2) {
+        if (etat_reçu_2 == true) {
+            etat = false;
         }
-        if (etat_reçu== false){
-            etat=true;
+        if (etat_reçu_2 == false) {
+            etat = true;
+        }
+
+    }
+
+    public boolean eteindreCellule(boolean etat_reçu_3) {
+        if (etat_reçu_3 == true) {
+            etat = false;
+        } else {
+            etat = false;
         }
         return etat;
     }
-    
+
+    public boolean estEteint(boolean etat_reçu_4) {
+        if (etat_reçu_4 == false) {
+            etat = true;
+        }
+        if (etat_reçu_4 == true) {
+            etat = false;
+        }
+        return etat;
+    }
+
+    public boolean getEtat(boolean etat_reçu_4) {
+        if (etat_reçu_4 == true) {
+            etat = true;
+        }
+        if (etat_reçu_4 == false) {
+            etat = false;
+        }
+        return etat;
+    }
+
+    @Override
+    public String toString() {
+        if (etat == true) {
+            return "O";
+        } else if (etat == false) {
+            return "X";
+        }
+    }
+
 }
