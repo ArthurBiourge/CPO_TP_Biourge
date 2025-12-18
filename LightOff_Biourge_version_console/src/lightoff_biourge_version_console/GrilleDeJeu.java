2f1389c9c99ad75f1e1226bf5c15009f23a25609
Arthur <arthur.biourge@epfedu.fr>
2025-12-18 16:01:28 +0100

    Mini Projet 2 "Lightoff": création de méthode de la classe "GrilleDeJeu": - Méthodes eteindreToutesLesCellules(), activerLigneColonneOuDiagonaleAleatoire(),melangerMatriceAleatoirement(int nbTours),activerLigneDeCellules(int idLigne) et activerColonneDeCellules(int idColonne)et activerDiagonaleDescendante() et activerDiagonaleMontante() créer et faites.

--- LightOff_Biourge_version_console/src/lightoff_biourge_version_console/GrilleDeJeu.java
+++ LightOff_Biourge_version_console/src/lightoff_biourge_version_console/GrilleDeJeu.java
@@ -4,6 +4,8 @@
  */
 package lightoff_biourge_version_console;
 
+import java.util.Random;
+
 /**
  *
  * @author utilisateur
@@ -15,8 +17,8 @@ public class GrilleDeJeu {
     int nbColonnes;
 
     public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
-        p_nbLignes = nbLignes;
-        p_nbColonnes = nbColonnes;
+        nbLignes = p_nbLignes;
+        nbColonnes = p_nbColonnes;
         for (int i = 0; i < nbLignes; i++) {
             grille[i] = new CelluleLumineuse[nbColonnes];
             for (int j = 0; j < nbColonnes; j++) {
@@ -25,7 +27,62 @@ public class GrilleDeJeu {
 
         }
     }
-    public void eteindreToutesLesCellules(){// on travail sur cette grille là et elle sera modifier dans la grille donc pas besoin de crée une nouvelle grille.
-    
+
+    public void eteindreToutesLesCellules() {// on travail sur cette grille là et elle sera modifier dans la grille donc pas besoin de crée une nouvelle grille.
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                grille[i][j].eteindreCellule();
+            }
+        }
     }
-}
+
+    public void activerLigneColonneOuDiagonaleAleatoire() {
+        int ligne_colone_diagonale;
+        int num_ligne_colone;
+        Random rand = new Random();
+        ligne_colone_diagonale = rand.nextInt(3);
+        num_ligne_colone = rand.nextInt(10);
+        if (ligne_colone_diagonale == 0) { //s'occupe d'allumé ou éteindre(si déja allumé) chaque case sur une ligne 
+            for (int j = 0; j < grille[nbColonnes].length; j++) {
+                grille[num_ligne_colone][j].activerCellule();
+            }
+        } else if (ligne_colone_diagonale == 1) {// colone
+            for (int i = 0; i < grille[nbLignes].length; i++) {
+                grille[i][num_ligne_colone].activerCellule();
+            }
+        } else if (ligne_colone_diagonale == 2) {//s'occupe d'allumé ou éteindre(si déja allumé) chaque case sur une
+            for (int h = 0; h < grille[nbLignes].length; h++) {
+                grille[h][h].activerCellule();
+            }
+        }
+    }
+
+    public void melangerMatriceAleatoirement(int nbTours) {
+        eteindreToutesLesCellules();
+        for (int k = 0; k < nbTours; k++) {
+            activerLigneColonneOuDiagonaleAleatoire();
+        }
+    }
+
+    public void activerLigneDeCellules(int idLigne) {
+        for (int j = 0; j < grille[nbColonnes].length; j++) {
+            grille[idLigne][j].activerCellule();
+        }
+    }
+
+    public void activerColonneDeCellules(int idColonne) {
+        for (int i = 0; i < grille[nbLignes].length; i++) {
+            grille[i][idColonne].activerCellule();
+        }
+    }
+
+    public void activerDiagonaleDescendante() {
+        for (int h = 0; h < grille[nbLignes].length; h++) {
+                grille[h][h].activerCellule();
+        
+    }}
+    public void activerDiagonaleMontante() {
+        for (int h = 0; h < grille[nbLignes].length; h++) {
+                grille[grille[nbLignes].length-h][grille[nbLignes].length-h].activerCellule();
+
+}}}
